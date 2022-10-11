a4d6c928203ff2328dec32464715af0bb21bcee4
SR-Dionizio <112962559+SR-Dionizio@users.noreply.github.com>
2022-10-11 16:10:59 -0300

    iniciando código para ativar estilo active com o teclado

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    console.log(idAudio);
 
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
 
-    console.log(contador);
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
+    
 }
