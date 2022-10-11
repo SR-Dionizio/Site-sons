38c7451e4e63a21428a4bc047e220743628bfe1c
Daniel Dionizio <112962559+SR-Dionizio@users.noreply.github.com>
2022-10-11 20:56:55 -0300

    condições por teclas adicionado

--- main.js
+++ main.js
@@ -15,8 +15,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function(){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(Event){
+        
+
+        if(Event.code  === 'Enter'|| Event.code  === 'Space'){
+            tecla.classList.add('ativa');
+        }
+        
     }
-    
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
